df286b0a879abdf38d10533ac43801e35c96dc1a
alexgdav <alexgdavidenko@gmail.com>
2019-11-30 20:54:24 -0500

    testing React Bootstrap install; changed button and nav color for test purposes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -76,7 +76,7 @@ class SignIn extends Component {
               />
             </Form.Group>
             <Button
-              variant="primary"
+              variant="info"
               type="submit"
             >
               Submit
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -89,7 +89,7 @@ class SignUp extends Component {
               />
             </Form.Group>
             <Button
-              variant="primary"
+              variant="info"
               type="submit"
             >
               Submit
