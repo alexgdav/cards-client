0b2326864eef3d997bbfec3761bf260f42cff297
alexgdav <alexgdavidenko@gmail.com>
2019-12-05 02:09:37 -0500

    Timer form clears Very hacky workaround with multiple divs

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -11,14 +11,28 @@ const Timer = () => {
   const reset = () => {
     setSeconds(60)
     setIsActive(false)
+    document.getElementById('seconds-seconds').style.visibility = 'hidden'
+    document.getElementById('seconds').style.visibility = 'visible'
   }
   const handleChange = event => {
     // event.persist()
     setSeconds(event.target.value)
   }
 
+  const setTheTime = () => {
+    document.getElementById('input').value = ''
+    document.getElementById('seconds').style.visibility = 'hidden'
+    document.getElementById('seconds-seconds').style.visibility = 'visible'
+  }
+
+  const resetTheTime = () => {
+    document.getElementById('seconds-seconds').style.visibility = 'hidden'
+    document.getElementById('seconds').style.visibility = 'visible'
+  }
+
   useEffect(() => {
     if (isActive && seconds === 0) {
+      resetTheTime()
       return toggle()
     } else {
       let interval
@@ -36,16 +50,17 @@ const Timer = () => {
   return (
     <div className="timer">
       <div className="time-input">
-        <label htmlFor="timer-input">Set Timer:</label>
-        <input name="timer-input" onChange={handleChange} style={{ width: '10rem', height: '2rem', margin: '1rem' }}/>
-        <h4 id="seconds">00:{seconds}s</h4>
+        <input id="input" name="timer-input" onChange={handleChange} style={{ width: '10rem', height: '2rem', margin: '1rem' }}/>
+        <button className="btn btn-secondary" onClick={setTheTime}>set timer</button>
+        <h4 id="seconds">00:00 seconds</h4>
+        <h4 id="seconds-seconds" style={{ visibility: 'hidden' }}>{seconds} seconds</h4>
       </div>
       <div className="row">
         <button style={{ margin: '1rem' }} className={`btn btn-success btn-success-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-          {isActive ? 'Stop Timer' : 'Start Timer'}
+          {isActive ? 'Pause Timer' : 'Start Timer'}
         </button>
         <button style={{ margin: '1rem' }} className="btn btn-warning" onClick={reset}>
-          Reset Timer
+          Stop & Reset Timer
         </button>
       </div>
     </div>
